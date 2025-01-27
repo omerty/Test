4d28a3815fe14b16847b4ba685ccf68ea1ef1b1f
Omer Mohiuddin <mdomermohi@gmail.com>
2025-01-26 19:22:53 -0500

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
-  plugins: [react()],
-});
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      onLog(level, log, handler) {
+        if (log.cause && log.cause.message === "Can't resolve original location of error.") {
+          return
+        }
+        handler(level, log)
+      }
+    }
+  }
+})
